e3cc6022e6e2b608ceced4d9ca7bd86fd898aebc
ss-cwill <108429400+ss-cwill@users.noreply.github.com>
2022-06-29 18:04:25 -0400

    Update app.py

--- SS - Proofpoint Admins/1.0.0/src/app.py
+++ SS - Proofpoint Admins/1.0.0/src/app.py
@@ -40,7 +40,9 @@ class ProofPoint(AppBase):
             'role': role
         }
         headers = {"Accept": "application/json"}
-        return requests.post(url_base,headers=headers,auth=(username,password),params=querystring,verify=False).json()
+        send_request = self.POST(url_base,headers=headers,auth=(username,password),params=querystring,verify=False)
+        return send_request
+        
 
 if __name__ == "__main__":
     ProofPoint.run()
